7575669270d986a12d44daec7ce29898fe088c94
Caio Marinho <caiomarinho8@gmail.com>
2015-04-20 12:06:14 -0300

    insert views

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,17 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
+
+    public static Result renderlogin(){
+        return ok(login.render());
+    }
+
+    public static Result renderRegistro(){
+        return ok(register.render());
+    }
+
+
 
 }
